2be44977d9d6ae1e996087dca1c549043513483c
Ronen Babayoff <ronen@lavaina.com>
2015-08-06 00:36:12 -0400

    Bump version to 0.1.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'practicalmeteor:mocha-console-reporter',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'A mocha package tests console reporter to be used with spacejam or standalone.',
   git: 'https://github.com/practicalmeteor/meteor-mocha-console-reporter.git',
   // By default, Meteor will default to using README.md for documentation.
@@ -10,10 +10,12 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
+  
   api.use(['coffeescript', "practicalmeteor:mocha"]);
-  api.imply("practicalmeteor:mocha");
-  api.addFiles('ConsoleReporter.coffee', 'client');
 
+  api.imply("practicalmeteor:mocha");
+
+  api.addFiles('ConsoleReporter.coffee', 'client');
 });
 
 Package.onTest(function(api) {
