be29925cc312777f23aa1fa917a8deb5ac7dc205
Juan Sepulveda <juan@lavaina.com>
2015-08-31 17:09:11 -0400

    Fix package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'practicalmeteor:mocha-console-reporter',
-  version: '0.1.2',
+  version: '0.1.3',
   summary: 'A mocha console reporter for running your package tests from the command line with spacejam.',
   git: 'https://github.com/practicalmeteor/meteor-mocha-console-reporter.git',
   // By default, Meteor will default to using README.md for documentation.
