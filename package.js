caac8753c272402fdecf28a16ecae381cd73f828
Juan Sepulveda <juan@lavaina.com>
2015-09-11 13:01:31 -0400

    Update package dependency

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'practicalmeteor:mocha-console-reporter',
-  version: '0.1.3',
+  version: '0.1.4',
   summary: 'A mocha console reporter for running your package tests from the command line with spacejam.',
   git: 'https://github.com/practicalmeteor/meteor-mocha-console-reporter.git',
   // By default, Meteor will default to using README.md for documentation.
@@ -10,8 +10,8 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
-  api.use(['coffeescript', "practicalmeteor:mocha@2.1.0_4"]);
-  api.imply("practicalmeteor:mocha@2.1.0_4");
+  api.use(['coffeescript', "practicalmeteor:mocha@2.1.0_5"]);
+  api.imply("practicalmeteor:mocha@2.1.0_5");
   api.addFiles('ConsoleReporter.coffee', 'client');
 
   api.addFiles('ConsoleReporter.coffee', 'client');
