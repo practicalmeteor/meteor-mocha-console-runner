e7faa6d398ae936b756916646bfb19130f041c15
Juan Sepulveda <juan@lavaina.com>
2016-03-14 22:56:17 -0400

    Add practicalmeteor:mocha as dependency again

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'practicalmeteor:mocha-console-reporter',
-  version: '0.1.3',
+  version: '0.2.0-meteor-1.3-beta.16.1',
   summary: 'A mocha console reporter for running your package tests from the command line with spacejam.',
   git: 'https://github.com/practicalmeteor/meteor-mocha-console-reporter.git',
   // By default, Meteor will default to using README.md for documentation.
@@ -10,9 +10,9 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
-  api.use(['coffeescript', "avital:mocha", 'practicalmeteor:loglevel', 'ecmascript']);
-  api.imply("avital:mocha");
+  api.versionsFrom("1.3-beta.16");
+  api.use(['coffeescript', "practicalmeteor:mocha@2.1.0-meteor-1.3-beta.16.1", 'practicalmeteor:loglevel', 'ecmascript']);
+  api.imply("practicalmeteor:mocha@2.1.0-meteor-1.3-beta.16.1");
   api.mainModule('ConsoleReporter.coffee', 'client');
 });
 
