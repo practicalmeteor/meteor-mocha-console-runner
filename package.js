0a38824dc0e4c61bc2303228a8e17770026c5dae
Ronen Babayoff <ronen@lavaina.com>
2016-03-31 00:43:32 -0400

    Fix github link in package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'practicalmeteor:mocha-console-runner',
   version: '0.2.0',
   summary: 'A mocha console reporter for running your package tests from the command line with spacejam.',
-  git: 'https://github.com/practicalmeteor/meteor-mocha-console-reporter.git',
+  git: 'https://github.com/practicalmeteor/meteor-mocha-console-runner.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',
