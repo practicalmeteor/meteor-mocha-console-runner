48e43494c9a2332bb13e5b1f31354143e7b014ae
Juan Sepulveda <juan@lavaina.com>
2016-04-11 15:54:32 -0400

    Bump version to v0.2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'practicalmeteor:mocha-console-runner',
-  version: '0.2.0-rc.4',
+  version: '0.2.0',
   summary: 'A mocha console reporter for running your package tests from the command line with spacejam.',
   git: 'https://github.com/practicalmeteor/meteor-mocha-console-runner.git',
   // By default, Meteor will default to using README.md for documentation.
