b21f214c22503111219d5d54a7644c079b7e8dbb
Juan Sepulveda <juan@lavaina.com>
2016-06-21 18:41:26 -0400

    Update practicalmeteor:mocha version and bump package version to 0.2.2-rc.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'practicalmeteor:mocha-console-runner',
-  version: '0.2.1',
+  version: '0.2.2-rc.1',
   summary: 'A mocha console reporter for running your package tests from the command line with spacejam.',
   git: 'https://github.com/practicalmeteor/meteor-mocha-console-runner.git',
   // By default, Meteor will default to using README.md for documentation.
@@ -11,9 +11,9 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom("1.3");
-  api.use(['coffeescript', "practicalmeteor:mocha@2.4.5_3", 'practicalmeteor:loglevel@1.2.0_2', 'ecmascript']);
+  api.use(['coffeescript', "practicalmeteor:mocha@=2.4.5-rc4.1", 'practicalmeteor:loglevel@1.2.0_2', 'ecmascript']);
   api.mainModule('ConsoleReporter.coffee', 'client');
-  api.imply("practicalmeteor:mocha@2.4.5_3");
+  api.imply("practicalmeteor:mocha@=2.4.5-rc4.1");
 });
 
 Package.onTest(function(api) {
